a6ba07c7bff0d5919fe578880ba8a32f4c98fab9
Krisna Restu Dewa <bartelsi.studio@gmail.com>
2024-06-01 07:29:38 +0700

    day 4

---
+++ 4-sentenceSmash.js
@@ -0,0 +1,9 @@
+// function smash (words) {
+//     return words.join(' ')
+//  };
+
+const smash = (words) => words.join(' ')
+
+ console.log (smash(["hello", "world"]));
+ console.log (smash(["hello", "amazing", "world"]));
+ console.log (smash(["this", "is", "a", "really", "long", "sentence"]));
